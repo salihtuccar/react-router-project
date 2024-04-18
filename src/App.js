e8c82629e1ea3028bd3610644f5ce9e178375131
salihtuccar <salihtuccar1@gmail.com>
2024-04-18 12:14:30 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import People from "./pages/People";
+import PersonDetail from "./pages/PersonDetail";
+import NotFound from "./pages/NotFound";
+import { Routes, Route } from "react-router-dom";
+import Paths from "./pages/Paths";
+import FullStack from "./pages/Fullstack";
+import Aws from "./pages/Aws";
+import ReactJS from "./pages/ReactJS";
+import Login from "./pages/Login";
+import PrivateRouter from "./pages/PrivateRouter";
+import { Navigate } from "react-router-dom";
+function App() {
+  return (
+    <>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/people" element={<People />} />
+        <Route path="/people/:id" element={<PersonDetail />} />
+
+        <Route path="/paths" element={<Paths />}>
+          <Route path="fullstack" element={<FullStack />}>
+            <Route path="react" element={<ReactJS />} />
+          </Route>
+          <Route path="aws" element={<Aws />} />
+        </Route>
+
+        <Route path="/contact" element={<PrivateRouter />}>
+          <Route path="" element={<Contact />} />
+        </Route>
+        <Route path="paths" element={<Paths />} />
+
+        {/* <Route path="/blog" element={<PrivateRouter />}>
+          <Route path="" element={<Blog />} />
+        </Route> */}
+
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+        {/* <Route path="*" element={<Navigate to="/" />} /> */}
+      </Routes>
+
+      <Footer />
+    </>
+  );
+}
+
+export default App;
